1724a75577db2f82caa926d8eb4af736ad746ed7
2shakib3 <2shakibhasan3@gmail.com>
2020-04-02 04:45:06 +0600

    landing about_3

---
+++ resources/views/layouts/frontend/footer.blade.php
@@ -0,0 +1,131 @@
+	   <footer class="footer mt-4">
+
+    	<div style="background-image:url('{{asset('frontend/image/footer.png')}}');background-repeat:no-repeat;background-size:cover;">
+    		<div class="row text-center">
+    			<h3></h3>
+    			<div class="col-md-3 map"  style="position: relative; top: 10px; left: 5%;">
+    				<div  style="padding-top:50px;">
+    				<p> <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.1480068715014!2d90.37202681445633!3d23.77774329364924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c74cd5bf5e7f%3A0xa7defae379e061d9!2sElectoral%20Training%20Institute%20(ETI)!5e0!3m2!1sen!2sbd!4v1583655582006!5m2!1sen!2sbd" width="300" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe> </p>
+    				</div>
+    			</div>
+
+    			<div class="col-md-3"  style=" margin:0px;">
+    				<h4>Featured Links</h4>
+    				<hr id="border1"> 
+
+    				<div id="links" style="padding-top:30px;">
+
+    					<i class="fa fa-angle-right" aria-hidden="true"></i>
+    					<a href="#" class="option">NID correction form-2</a><br>
+
+    					<i class="fa fa-angle-right" aria-hidden="true"></i>
+    					<a href="#" class="option">NID Wing</a><br>
+
+    					<i class="fa fa-angle-right" aria-hidden="true"></i>
+    					<a href="#" class="option">National Web Portal</a><br>
+
+    					<i class="fa fa-angle-right" aria-hidden="true"></i>
+    					<a href="#" class="option">BPATC</a><br>
+
+						<i class="fa fa-angle-right" aria-hidden="true"></i>
+    					<a href="#" class="option">BCS Admin Academy</a>
+    				
+    				</div>
+    				
+    			</div>
+
+    			
+
+
+    			<div class="col-md-3"  style="margin:0px; padding:0px;">
+    				<h4>Form Download</h4>
+    				<hr id="border2">
+    				
+    					<div class="line-1">
+
+    					<div class="row" style="margin:0px; padding:0px;">
+    						<div class="col-md-2" style="padding:0px;">	
+    						<i class="fa fa-file-text-o" id="icon1" aria-hidden="true"></i></div> 
+    						<div class="col-md-8">
+    						<p style=" padding:0px;">NID correction form-2</p></div> 
+    						<div class="col-md-1" style="padding:0px;">
+    						<a href="#" class="btn btn-info" id="download" role="button">Download</a></div>
+    					</div>
+    				</div>
+
+    					<div class="line-1">
+    						<div class="row" style="margin:0px; padding:0px;">
+    						<div class="col-md-2" style="padding:0px;">
+    						<i class="fa fa-file-text-o" id="icon1" aria-hidden="true"></i> </div> 
+    						<div class="col-md-8">
+    						<p style=" padding:0px;">Voter transfer form</p> </div> 
+    						<div class="col-md-1" style="margin:0px;">
+    						<a href="#" class="btn btn-info" id="download" role="button">Download</a> </div>
+    					</div>
+    				</div>
+
+    					<div class="line-1">
+    						<div class="row" style="margin:0px; padding:0px;">
+    						<div class="col-md-2" style="padding:0px;">
+    						<i class="fa fa-file-text-o" id="icon1" aria-hidden="true"></i> </div> 
+    						<div class="col-md-8">
+    						<p style=" padding:0px;">NID correction form-2</p> </div>
+    						<div class="col-md-1" style="padding:0px;">
+    						<a href="#" class="btn btn-info" id="download" role="button">Download</a> </div>
+    					</div>
+    				</div>
+
+    				<!--<div id="verticle-line1"> </div>-->
+    			</div>
+
+
+
+    			<div class="col-md-3" style="margin:0px;">
+    				<h4>Find Us</h4>
+    				<hr id="border3">
+    				<div>
+    					<div class="row">
+    						<div class="col-md-2">
+    					<i class="fa fa-envelope-o" id="icon1" aria-hidden="true"></i></div> 
+    					<div class="col-md-9">
+    					<p>dg_eti@yahoo.com</p></div>
+    					</div>
+
+    						<div class="row">
+    						<div class="col-md-2">
+    					<i class="fa fa-phone" id="icon1" aria-hidden="true"></i> </div>
+    						<div class="col-md-9">
+    					<p> +88 02 8181 731 </p></div>
+    					</div>
+
+    					<div class="row">
+    						<div class="col-md-2">
+    					<i class="fa fa-file-text-o" id="icon1" aria-hidden="true"></i></div>
+    				
+    				<div class="col-md-9">
+    					<p> Plot-E, 14/Z, Agargoan,<br>Dhaka-1207</p></div>
+    				</div>
+    			</div>
+    		</div>
+
+    	</div>
+
+    	<div class="row" style=" text-align:center; margin-top:150px; margin-left:180px;">
+    		<div class="col-md-12">
+    			<i class="fa fa-envelope-o" id="icon1" aria-hidden="true"></i>
+    			<i class="fa fa-envelope-o" id="icon1" aria-hidden="true"></i>
+    			<i class="fa fa-envelope-o" id="icon1" aria-hidden="true"></i>
+
+    		</div>
+    	</div>
+
+    	<div class="row" style="float:right;">
+    		<div class="col-md-12">
+    			<p>copyright text .....</p>
+    		</div>
+
+    	</div>
+
+
+        
+    </footer> 
